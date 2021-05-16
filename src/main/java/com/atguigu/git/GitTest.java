b01c82687b4246753d782a39f19fc4479d919a2a
web123bin <web123bin@gmail.com>
2021-05-16 19:57:58 +0800

    master commit3

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("master commit4");
         System.out.println("master commit4");
+        System.out.println("master commit4");
     }
 }
